9823cddf51367d2c71d752085f01c67d7a57fb08
Kajal <kajal814900@gmail.com>
2024-07-13 15:52:38 +0530

    fix a small bug

--- QrGenerator/QR.js
+++ QrGenerator/QR.js
@@ -15,20 +15,3 @@ function generateQR() {
 generateBtn.addEventListener('click', generateQR);
 });
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     const qrText = document.getElementById('qrText');
-//     const qrImage = document.getElementById('qrImage');
-//     const generateBtn = document.getElementById('generateBtn');
-
-//     function generateQR() {
-//         const qrValue = qrText.value;
-//         if (qrValue.trim() === "") {
-//             alert("Please enter a valid URL or text");
-//             return;
-//         }
-//         qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + encodeURIComponent(qrValue);
-//         qrImage.style.display = "block";
-//     }
-
-//     generateBtn.addEventListener('click', generateQR);
-// });
